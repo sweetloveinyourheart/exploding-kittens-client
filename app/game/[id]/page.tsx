92b12328576a48281b9a44e79e36fe268e21def3
Ryan Nguyen <sweetloveinyourheart@gmail.com>
2025-05-25 22:18:31 +0700

    Update UI

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -5,6 +5,8 @@ import { HOME_ROUTER } from "@/constants/routers"
 import GamePlay from "@/features/game/game"
 import { grpcServer } from "@/lib/grpc/grpc-server"
 import { redirect } from "next/navigation"
+import Image from "next/image"
+import Table from "@/assets/images/table.png"
 
 interface GamePageProps {
     params: Promise<{ [key: string]: string | string[] | undefined }>
@@ -34,12 +36,22 @@ export default async function GamePage({ params }: GamePageProps) {
     const players = getPlayerRes.data.users
 
     return (
-        <>
+        <div className="overflow-hidden">
+            {/* Background Image */}
+            <div className="absolute inset-0 -z-10">
+                <Image
+                    src={Table}
+                    alt="Exploding Kittens Background"
+                    fill
+                    priority
+                    className="object-cover object-center"
+                />
+            </div>
             <GamePlay
                 gameId={meta.gameId}
                 userId={session.user.userId}
                 players={JSON.stringify(players)}
             />
-        </>
+        </div>
     )
 }
